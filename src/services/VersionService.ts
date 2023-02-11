0ed528dd8461fd3988d9e1904d4dee745c2913ee
jangot <pulin.pavel@gmail.com>
2023-02-11 18:08:25 +0100

    sort versions

--- src/services/VersionService.ts
+++ src/services/VersionService.ts
@@ -20,9 +20,11 @@ export class VersionService {
             return keys.map((key) => versions.find((ver) => ver.id == key));
         });
         this.loaderListByApplication = new DataLoader(async(keys: Array<string>) => {
-            const versions = await this.db.getRepository(VersionEntitie).find({
-                where: { applicationId: In(keys) },
-            });
+            const versions = await this.repo
+                .createQueryBuilder('versions')
+                .where('versions.applicationId In(:...keys)', { keys: keys.map(Number) })
+                .orderBy('string_to_array(version, \'.\')::int[]', 'DESC')
+                .getMany();
 
             return keys.map((key) => versions.filter((ver) => ver.applicationId == key));
         });
@@ -30,6 +32,9 @@ export class VersionService {
 
     findList(): Promise<VersionEntitie[]> {
         return this.repo.find({});
+            // .createQueryBuilder('versions')
+            // .orderBy("string_to_array(version, '.')::int[]")
+            // .getMany();
     }
 
     create(input: VersionInput): Promise<VersionEntitie> {
