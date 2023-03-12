eeab2e7d4a2ad2503441440284bb84456fb014cc
jangot <pulin.pavel@gmail.com>
2023-03-12 13:25:48 +0100

    retrive env with versions

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,20 +1,14 @@
 import { Router } from 'express';
 import { Services } from 'src/services';
 import { ServerContext } from '../server'
+import { VersionRetriverService } from '../services/VersionRetriverService'
 
 interface ApiOprtions {
     services: Services;
     serverContext: ServerContext;
 }
 
-// {
-//     "env": "Navigation testing users",
-//     "value": "444",
-//     "key": "user",
-//     "version": "1.2.4",
-//     "app": "audit-logs",
-//     "created": "2022-11-13T17:07:28.139Z"
-// }
+
 
 export function getApiRouter({ services, serverContext }: ApiOprtions): Router {
     const apiRouter = Router();
@@ -22,51 +16,36 @@ export function getApiRouter({ services, serverContext }: ApiOprtions): Router {
     apiRouter.get('/application/:name', async(req, res) => {
         const { name } = req.params;
         const entiry = await services.application.repo.findOneBy({ name });
-        const query = await serverContext.db
-            .createQueryBuilder()
-
-            .select('e.name', 'env')
-            .addSelect('e.id', 'id')
-            .addSelect('rk.name', 'key')
-            .addSelect('r.value', 'value')
-            .addSelect('d.created_at', 'created')
-            .addSelect('v.version', 'version')
-            .addSelect('a.name', 'app')
-
-            .from('environment', 'e')
-            .innerJoin('rule', 'r', 'r.environment_id = e.id')
-            .innerJoin('rule_key', 'rk', 'rk.id = r.key_id')
-            .innerJoin('deploy', 'd', 'd.environment_id = e.id')
-            .innerJoin('version', 'v', 'v.id = d.version_id')
-            .innerJoin('application', 'a', 'a.id = v.application_id')
-
-            .where('a.name = :appName', { appName: name })
-            .orderBy('e.order_index', 'DESC')
-            .addOrderBy('e.name')
-            .addOrderBy('d.created_at')
-
-            .execute()
-
-        const result = query.reduce((memo, item) => {
-            // TODO add env orders
-            memo[item.id] = memo[item.id] || { name: item.env };
-
-            memo[item.id].rules = memo[item.id].rules || {};
-            memo[item.id].rules[item.key] = memo[item.id].rules[item.key] || [];
-
-            if (!memo[item.id].rules[item.key].includes(item.value)) {
-                memo[item.id].rules[item.key].push(item.value);
-            }
-
-            memo[item.id].versions = memo[item.id].versions || [];
-            if (!memo[item.id].versions.includes(item.version)) {
-                memo[item.id].versions.push(item.version);
-            }
-
-            return memo;
-        }, {});
+        const service = new VersionRetriverService(serverContext.db);
+        const result = await service.retrive(name);
 
         res.send({ entiry, result });
+    });
+
+    apiRouter.get('/app/:name', async(req, res) => {
+        const { name } = req.params;
+
+        const env = await services.environment.repo.find({
+            relations: {
+                rules: {
+                    key: true
+                },
+                deploys: {
+                    version: {
+                        application: true
+                    }
+                }
+            },
+            where: {
+                deploys: {
+                    version: {
+                        application: { name }
+                    }
+                }
+            }
+        });
+
+        res.send({ env });
     });
 
     return apiRouter;
---
+++ src/services/VersionRetriverService.ts
@@ -0,0 +1,62 @@
+import { DataSource } from 'typeorm';
+
+// {
+//     "env": "Navigation testing users",
+//     "value": "444",
+//     "key": "user",
+//     "version": "1.2.4",
+//     "app": "audit-logs",
+//     "created": "2022-11-13T17:07:28.139Z"
+// }
+
+export class VersionRetriverService {
+    constructor(private db: DataSource) {}
+
+    async retrive(name: string) {
+        const query = await this.db
+            .createQueryBuilder()
+
+            .select('e.name', 'env')
+            .addSelect('e.id', 'id')
+            .addSelect('rk.name', 'key')
+            .addSelect('r.value', 'value')
+            .addSelect('d.created_at', 'created')
+            .addSelect('v.version', 'version')
+            .addSelect('a.name', 'app')
+
+            .from('environment', 'e')
+            .innerJoin('rule', 'r', 'r.environment_id = e.id')
+            .innerJoin('rule_key', 'rk', 'rk.id = r.key_id')
+            .innerJoin('deploy', 'd', 'd.environment_id = e.id')
+            .innerJoin('version', 'v', 'v.id = d.version_id')
+            .innerJoin('application', 'a', 'a.id = v.application_id')
+
+            .where('a.name = :appName', { appName: name })
+            .orderBy('e.order_index', 'DESC')
+            .addOrderBy('e.name')
+            .addOrderBy('d.created_at')
+
+            .execute();
+
+            const result = query.reduce((memo, item) => {
+                // TODO add env orders
+                memo[item.id] = memo[item.id] || { name: item.env };
+
+                memo[item.id].rules = memo[item.id].rules || {};
+                memo[item.id].rules[item.key] = memo[item.id].rules[item.key] || [];
+
+                if (!memo[item.id].rules[item.key].includes(item.value)) {
+                    memo[item.id].rules[item.key].push(item.value);
+                }
+
+                memo[item.id].versions = memo[item.id].versions || [];
+                if (!memo[item.id].versions.includes(item.version)) {
+                    memo[item.id].versions.push(item.version);
+                }
+
+                return memo;
+            }, {});
+
+            return result;
+    }
+}
